96921331d2e103118a2e7c0ad34205c39ac640af
AlinaChorna7 <159713721+AlinaChorna7@users.noreply.github.com>
2024-06-10 16:21:36 +0200

    add initial files

--- js/task-6.js
+++ js/task-6.js
@@ -1,31 +1,49 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const input = document.querySelector('#controls input');
-  const createButton = document.querySelector('[data-create]');
-  const destroyButton = document.querySelector('[data-destroy]');
+function getRandomHexColor(){
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6,0)
+  }`;
+}
+function createBoxes(amount){
   const boxesContainer = document.getElementById('boxes');
-
-  createButton.addEventListener('click', () =>{
-    const amount = parseInt(input.value, 10);
-    if(isNaN(amount) || amount < 1 || amount > 100){
-    return alert('Please enter a number between 1 and 100.');
-    }
+  boxesContainer.innerHTML = '';
+  const fragment = document.createDocumentFragment()
+  let size = 30;
+  for (let i = 0; i < amount; i++){
+    const box = document.createElement('div');
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+   fragment.appendChild(box);
+    size += 10;
+  }
+  boxesContainer.appendChild(fragment);
+}
+function destroyBoxes (){
+  const boxesContainer = document.getElementById('boxes');
+  boxesContainer.innerHTML= '';
+}
+document.querySelector('[data-create]').addEventListener('click', () =>{
+  const input = document.querySelector('#controls input');
+  const amount = parseInt(input.value);
+  if (amount >=1 && amount <= 100){
     createBoxes(amount);
     input.value = '';
-  });
-  destroyButton.addEventListener('click', () =>{
-    boxesContainer.innerHTML = '';
-
-  });
-function createBoxes(amount){
-  boxesContainer.innerHTML = '';
-  for(let i = 0; i < amount; i ++){
-    const box = document.createElement('div');
-    box.style.backgroundColor = getRandomHexColor();
-    boxesContainer.appendChild(box);
+  } else{
+    alert('Please enter a number between 1 and 100');
+    
   }
-}
-
-function getRandomHexColor(){
-  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
-}
 });
+document.querySelector('[data-destroy]').addEventListener('click', () => {
+destroyBoxes()
+});
+
+
+
+
+
+
+
+
+
+
